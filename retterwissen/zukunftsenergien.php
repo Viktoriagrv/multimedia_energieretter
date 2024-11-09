0cd9b8bcd243afd0c5077351b544ea68595554e3
SabrinaSiebels <sabrina.siebels@student.jade-hs.de>
2024-11-09 16:20:41 +0100

    Create zukunftsenergien.php

---
+++ retterwissen/zukunftsenergien.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <title>Zukunftsenergien</title>
+
+    <!-- Bootstrap CSS für Styling und Komponenten einbinden -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- Einbinden der Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+
+    <!-- Einbinden der CSS-Datei -->
+    <link rel="stylesheet" href="include/css/styles.css"> <!-- Pfad zu deiner CSS-Datei -->
+
+    <!-- Einbinden der Navigationsleiste -->
+    <?php include '../include/partials/nav.php'; ?>
+</head>
+
+<body>
+    <main>
+       <div>
+		   <a href="zukunftsenergien/solar.php">Solarenergie</a>
+		   
+	   </div> 
+    </main>
+
+    <!-- Einbinden des Footers -->
+    <?php include '../include/partials/footer.php'; ?>
+
+    <!-- Bootstrap JS und Abhängigkeiten für Dropdown und Toggler -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+    <!-- Einbinden der externen JavaScript-Datei -->
+    <script src="../include/js/intro.js"></script>
+</body>
+</html>
