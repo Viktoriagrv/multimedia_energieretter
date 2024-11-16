cc3a6ac389f9a9b36ae61ee415e7fddd0b362a76
SabrinaSiebels <sabrina.siebels@student.jade-hs.de>
2024-11-16 19:59:13 +0100

    Anlegen Impressumsseite

---
+++ impressum/impressum.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <title>Impressum</title>
+
+       <!-- Einbinden der Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Open+Sans:wght@400&display=swap" rel="stylesheet">
+    <!-- Einbinden der CSS-Datei -->
+    <link rel="stylesheet" href="include/css/styles.css"> <!-- Pfad zu deiner CSS-Datei -->
+
+    
+    <?php include'include/partials/nav.php'; ?>				<!-- Einbinden der Navigationsleiste -->
+	<?php include 'include/partials/footer.php'; ?>			<!-- Einbinden des Footers -->
+	
+</head>
+
+<body>
+    <main>
+        <div>
+            <!-- Hier könnte Ihr Hauptinhalt stehen -->
+			<p> Hallo!</p>
+        </div>
+    </main>
+
+    
+   
+    
+    <!-- Einbinden der externen JavaScript-Datei -->
+    <script src="include/js/intro.js"></script>
+</body>
+</html>
