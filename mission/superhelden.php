17905a0c6c14ead5e6398531dfd41dba9f73933f
SabrinaSiebels <sabrina.siebels@student.jade-hs.de>
2024-11-29 11:35:39 +0100

    Update superhelden.php

--- mission/superhelden.php
+++ mission/superhelden.php
@@ -11,7 +11,7 @@
 				
 		.Schriftzug{
 			position: fixed;
-			top: center;
+			top: 35%;
 			left: 3rem;
 			font-family: "bangers";
 			font-size: 5rem;
@@ -102,46 +102,46 @@
 		
 		.SCARLETSHADE {
 			position: absolute;
-			top: 50px;
-			left: 500px;
+			top: 5%;
+			left: 40%;
 			}
 		
 		.GOLDENKNIGHT {
 			position: absolute;
-			top: 50px;
-			left: 750px;
+			top: 5%;
+			left: 60%;
 			}
 		
 		.GREENBLAZE {
 			position: absolute;
-			top: 50px;
-			left: 1000px;
+			top: 5%;
+			left: 80%;
 			}
 		
 		.THUNDERBOLT {
 			position: absolute;
-			top: 300px;
-			left: 500px;
+			top: 40%;
+			left: 40%;
 			}
 		
 		.CYBERTITAN {
 			position: absolute;
-			top: 300px;
-			left: 750px;
+			top: 40%;
+			left: 60%;
 			}
 		
 		.SONICSHIELD {
 			position: absolute;
-			top: 300px;
-			left: 1000px;
+			top: 40%;
+			left:80%;
 			}
 		
 		.INFOSCARLETSHADE {
 			flex-direction: row;
 			position: sticky;
 			padding: 30px;
-			top: 200px;
-			left: 650px;
+			top: 5%;
+			left: 30%;
 			height:400px;
 			width: 650px;
 			border-radius: 20px 20px 20px 20px;
@@ -157,8 +157,8 @@
 			flex-direction: row;
 			position: sticky;
 			padding: 30px;
-			top: 200px;
-			left: 650px;
+			top: 5%px;
+			left: 30%px;
 			height:400px;
 			width: 650px;
 			border-radius: 20px 20px 20px 20px;
@@ -174,8 +174,8 @@
 			flex-direction: row;
 			position: sticky;
 			padding: 30px;
-			top: 200px;
-			left: 650px;
+			top: 5%;
+			left: 30%;
 			height:400px;
 			width: 650px;
 			border-radius: 20px 20px 20px 20px;
@@ -191,8 +191,8 @@
 			flex-direction: row;
 			position: sticky;
 			padding: 30px;
-			bottom: 200px;
-			left: 650px;
+			top: 5%;
+			left: 30%;
 			height:400px;
 			width: 650px;
 			border-radius: 20px 20px 20px 20px;
@@ -208,8 +208,8 @@
 			flex-direction: row;
 			position: sticky;
 			padding: 30px;
-			top: 200px;
-			left: 650px;
+			top: 5%;
+			left: 30%;
 			height:400px;
 			width: 650px;
 			border-radius: 20px 20px 20px 20px;
@@ -225,8 +225,8 @@
 			flex-direction: row;
 			position: sticky;
 			padding: 30px;
-			bottom: 200px;
-			left: 650px;
+			top: 5%;
+			left: 30%;
 			height:400px;
 			width: 650px;
 			border-radius: 20px 20px 20px 20px;
@@ -282,17 +282,20 @@
 
 <body>
 	
-	<!-- Einbinden der Navigationsleiste -->
-	<?php include '../include/partials/nav.php'; ?>
+	<!-- Hintergrundvideo -->
+	<video autoplay muted loop id="backgroundVideo">
+		<source src="bilder_retterwissen/background_video.mp4" type="video/mp4">
+		<source src="video/background_video.webm" type="video/webm">
+	</video>
 	
-	<main>
+	
 
 			<div class="Schriftzug">
 				WÄHLE DEINEN <br/> SUPERHELDEN
 			</div>
 			
 		
-			<div class="BILDBOX">
+			
 				<div class="SCARLETSHADE">
 					<img class="BILD" src="../images/BadGirl.png" alt="Scarlet Shade">
 				</div>
@@ -304,7 +307,7 @@
 				<div class="GREENBLAZE">
 					<img class="BILD" src="../images/Prinzessin.png" alt="Green Blaze">
 				</div>
-			</div>  
+			 
 
 			<div>
 				<div class="THUNDERBOLT">
@@ -416,7 +419,7 @@
 				<button class="AUSWAHL" onclick="window.location.href='missionsstart.php'">Auswählen</button>
 			</div>
 		</div>
-		</main>
+		
 	
 			<script>
 
