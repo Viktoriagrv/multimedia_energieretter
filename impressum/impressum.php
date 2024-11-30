b80fc21558576f6791e380ab59bf0f95df1bb088
Viktoria Grushev <126667727+Viktoriagrv@users.noreply.github.com>
2024-11-30 14:17:40 +0100

    Impressum und Missionstart

--- impressum/impressum.php
+++ impressum/impressum.php
@@ -4,29 +4,147 @@
     <meta charset="utf-8">
     <title>Impressum</title>
 
-       <!-- Einbinden der Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Open+Sans:wght@400&display=swap" rel="stylesheet">
-    <!-- Einbinden der CSS-Datei -->
-    <link rel="stylesheet" href="include/css/styles.css"> <!-- Pfad zu deiner CSS-Datei -->
-
     
-    <?php include'include/partials/nav.php'; ?>				<!-- Einbinden der Navigationsleiste -->
-	<?php include 'include/partials/footer.php'; ?>			<!-- Einbinden des Footers -->
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Open+Sans:wght@400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="include/css/styles.css">
 	
+	
+	<style>	
+		
+		.STARTBUTTON{
+			position: sticky;
+			justify-content: center;
+			background-color: transparent;
+			font-family:  "bangers";
+			font-size: 12rem;
+			line-height: 1.2;
+			color: #e8a723;
+			border: none;
+			top: 5rem;
+			align-items: center;
+			text-align: center;
+			background-color: transparent;
+			border: 0;
+			transition: all 1s;
+			width:800px;
+			}
+		
+		.KLEIN{
+			position: relative;
+			top: 2rem;
+			font-size: 5rem;
+			
+			}	
+		
+
+		#scrolltext {
+			position: relative;
+			justify-content: center;
+			align-items:center;
+			top: 4rem;
+			width:800px;
+			height:400px;
+			background-color:transparent;
+			font-family:verdana;
+			font-size: 2rem;
+			color:#FFFFFF;
+			text-align: justify;
+			overflow:hidden;
+			}
+		
+		
+		.scroll{
+			visibility: hidden;
+			}
+		
+		.ZEIGEN{
+			visibility: visible;
+			}
+			
+		</style>
+
+
 </head>
 
 <body>
-    <main>
-        <div>
-            <!-- Hier könnte Ihr Hauptinhalt stehen -->
-			<p> Hallo!</p>
-        </div>
-    </main>
+       <!-- Hintergrundvideo -->
+<video autoplay muted loop id="backgroundVideo">
+    <source src="retterwissen/bilder_retterwissen/background_video.mp4" type="video/mp4">
+    <source src="retterwissen/bilder_retterwissen/background_video.mp4" type="video/webm">
+</video>
 
+
+    <!-- Navigation einbinden -->
+  <nav class="navbar-custom">
+
+    <!-- Branding -->
+    <a class="navbar-brand" href="../../mission/missionsstart.php">Die Energieretter</a>
     
-   
-    
-    <!-- Einbinden der externen JavaScript-Datei -->
-    <script src="include/js/intro.js"></script>
+    <!-- Hauptmenü -->
+    <div class="menu">
+
+       
+        <a href="../../index.php" class="menu-link menu-item">Vorspann</a>
+
+        
+        <div class="dropdown menu-item">
+            <button class="dropbtn menu-link">Mission</button>
+            <div class="dropdown-content">
+                <a href="../../mission/superhelden.php">Die Superhelden</a>
+                <a href="../../mission/missionsstart.php">Missionsstart</a>
+                <a href="../../mission/codeeingabe.php">Code-Eingabe</a>
+            </div>
+        </div>
+        
+        <!-- Dropdown-Menü -->
+        <div class="dropdown menu-item" style="margin-right: 50px;">
+            <button class="dropbtn menu-link">Retterwissen</button>
+            <div class="dropdown-content">
+                <a href="../../retterwissen/zukunftsenergien.php">Zukunftsenergie</a>
+                <a href="../../retterwissen/innovationen.php">Innovationen</a>
+                <a href="../../retterwissen/klimaschaeden.php">Klimaschäden</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+	
+	<main>
+		
+			<div>
+				<button id="START" class="STARTBUTTON">Die Welt ist <br> in Gefahr!</button>
+
+
+				<div class="scroll" id="scrolltext"> 
+					<p><br><br>Wir befinden uns im Jahr 2050. In der Vergangenheit haben die Menschen viel zu wenig Zunkunftsenergien genutzt. Die fossilen Brennstoffe haben unsere Umwelt verschmutzt. Nun breitet sich die Dunkelheit aus und nur die mutigsten Helden können uns retten. Steht auf, ihr Superhelden da draußen! Es ist Zeit zu kämpfen! Wähle dir einen Verbündeten und reise mit ihm zusammen in die Vergangenheit, um unsere Zukunft zu retten. Wir müssen die Hoffnung bewahren und die Energien bündeln. Gemeinsam Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+					</p>
+					 <img src="images/weltkugel.png" alt="Weltkugel" style="width: 420px; height: auto;">
+				</div>
+	
+			</div>
+	</main>
+	
+	
+	<script> 
+		
+		document.querySelector("#START").addEventListener("click",TextStarten);
+
+				
+		function TextStarten() {
+			document.querySelector(".STARTBUTTON").classList.add("KLEIN");
+			document.querySelector(".scroll").classList.add("ZEIGEN");
+			document.querySelector("#scrolltext").scrollTop -=-1;
+			timer1 = setTimeout('TextStarten("'+scrolltext+'", '+-1+')', "60");
+			}
+			
+	</script>
+
+	
+	
+	<footer class="footer-custom">
+    <div class="footer-links">
+        <a href="../../impressum/impressum.php">Impressum</a>
+    </div>
+</footer>
 </body>
 </html>
--- mission/missionsstart.php
+++ mission/missionsstart.php
@@ -2,81 +2,108 @@
 <html lang="de">
 <head>
     <meta charset="utf-8">
-    <title>Missionsstart</title>
+    <title>Missions-Start</title>
 
     
-    <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Open+Sans:wght@400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../include/css/styles.css">
-	
-	
-	<style>	
-		
 
-		</style>
+    <!-- Einbinden der Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
 
+    <!-- Einbinden der CSS-Datei -->
+    <link rel="stylesheet" href="include/css/styles.css">
 
-</head>
-
-<body>
-       <!-- Hintergrundvideo -->
-<video autoplay muted loop id="backgroundVideo">
-    <source src="../retterwissen/bilder_retterwissen/background_video.mp4"type="video/mp4">
-    <source src="../retterwissen/bilder_retterwissen/background_video.mp4" type="video/webm">
-</video>
-
-
-    <!-- Navigation einbinden -->
-  <nav class="navbar-custom">
-
+    <!-- Einbinden der Navigationsleiste -->
+    <nav class="navbar-custom">
     <!-- Branding -->
     <a class="navbar-brand" href="../../mission/missionsstart.php">Die Energieretter</a>
     
-    <!-- Hauptmenü -->
+    <!-- Navigation Menu -->
     <div class="menu">
-
-       
-        <a href="../../index.php" class="menu-link menu-item">Vorspann</a>
-
-        
-        <div class="dropdown menu-item">
-            <button class="dropbtn menu-link">Mission</button>
+        <a href="../../index.php" class="menu-link yellow-bg green-text">Vorspann</a>
+        <div class="dropdown">
+            <button class="dropbtn yellow-bg green-text">Mission</button>
             <div class="dropdown-content">
-                <a href="../../mission/superhelden.php">Die Superhelden</a>
-                <a href="../../mission/missionsstart.php">Missionsstart</a>
-                <a href="../../mission/codeeingabe.php">Code-Eingabe</a>
+                <a href="../../mission/superhelden.php" class="green-text">Die Superhelden</a>
+                <a href="../../mission/missionsstart.php" class="green-text">Missionsstart</a>
+                <a href="../../mission/codeeingabe.php" class="green-text">Code-Eingabe</a>
             </div>
         </div>
-        
-        <!-- Dropdown-Menü -->
-        <div class="dropdown menu-item" style="margin-right: 50px;">
-            <button class="dropbtn menu-link">Retterwissen</button>
+        <div class="dropdown">
+            <button class="dropbtn yellow-bg green-text">Retterwissen</button>
             <div class="dropdown-content">
-                <a href="../../retterwissen/zukunftsenergien.php">Zukunftsenergie</a>
-                <a href="../../retterwissen/innovationen.php">Innovationen</a>
-                <a href="../../retterwissen/klimaschaeden.php">Klimaschäden</a>
+                <a href="../../retterwissen/zukunftsenergien.php" class="green-text">Zukunftsenergie</a>
+                <a href="../../retterwissen/innovationen.php" class="green-text">Innovationen</a>
+                <a href="../../retterwissen/klimaschaeden.php" class="green-text">Klimaschäden</a>
             </div>
         </div>
     </div>
 </nav>
 
+
 	
-	<main>
+	<style>
+		/*.NEXT{flex-direction: row;
+			}
 		
+		.HERO{
+			justify-content: center;
+			flex-grow: 1;
+			position: sticky;
+			top: 15rem;
+			margin: 5rem;
+			}
+		
+		.WELTBLAU{
+			justify-content: flex-start;
+			top: 15rem;
+			flex-grow: 2;
 			
-	</main>
-	
-	
-	<script> 
+			}*/
+		
+		.SPRECHBLASE {
+			justify-content: flex-end;
+			flex-grow: 1;
+			position: sticky;
+			padding: 30px;
+			top: 15rem;
+			height:400px;
+			width: 650px;
+			border-radius: 70px 70px 70px 0px;
+			background-color: white;
+			box-shadow: 5px 5px 10px 0px gray;
+			visibility: visible;
+			}		
 
+	</style>
+</head>
+
+<body>
+    <main>
+		
+		<div class="NEXT">
+			<div class="WELTBLAU">
+				<img src="../images/Weltkarte_blau.png" alt="Weltkarte in hellblau">
+			</div>
+			<div class="HERO">
+				<img src="../images/AquaBoy.png"
+			</div>
+
+			<div class="SPRECHBLASE">
+				Hallo!
+			</div>	
+		</div>
 			
-	</script>
+    </main>
 
-	
-	
-	<footer class="footer-custom">
+    <!-- Einbinden des Footers -->
+    <footer class="footer-custom">
     <div class="footer-links">
         <a href="../../impressum/impressum.php">Impressum</a>
     </div>
 </footer>
+   
+    
+    
 </body>
 </html>
+
