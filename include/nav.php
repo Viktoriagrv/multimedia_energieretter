b0eb0f6ec4b8de136ab19605f53212ad985a1719
SabrinaSiebels <sabrina.siebels@student.jade-hs.de>
2024-12-16 14:48:21 +0100

    Nav bar

--- include/nav.php
+++ include/nav.php
@@ -4,24 +4,24 @@
     
     <!-- Navigation Menu -->
     <div class="menu">
-        <a href="https://mwj-mm-05.kurs.jade-hs.de/index.php" class="menu-link yellow-bg green-text">Vorspann</a>
+        <a href="https://mwj-mm-05.kurs.jade-hs.de/index.php" class="UEBERSCHRIFT">Vorspann</a>
         <div class="dropdown">
-            <button class="dropbtn yellow-bg green-text">Mission</button>
+            <button class="UEBERSCHRIFT">Mission</button>
             <div class="dropdown-content">
-                <a href="https://mwj-mm-05.kurs.jade-hs.de/mission/missionsstart.php" class="green-text">Missionsstart</a>
-				<a href="https://mwj-mm-05.kurs.jade-hs.de/mission/superhelden.php" class="green-text">Die Superhelden</a>
-                <a href="https://mwj-mm-05.kurs.jade-hs.de/mission/codeeingabe.php" class="green-text">Spiel fortsetzen</a>
+                <a href="https://mwj-mm-05.kurs.jade-hs.de/mission/missionsstart.php" class="UNTERUEBERSCHRIFT">Missionsstart</a>
+				<a href="https://mwj-mm-05.kurs.jade-hs.de/mission/superhelden.php" class="UNTERUEBERSCHRIFT">Die Superhelden</a>
+                <a href="https://mwj-mm-05.kurs.jade-hs.de/mission/codeeingabe.php" class="UNTERUEBERSCHRIFT">Spiel fortsetzen</a>
             </div>
         </div>
         <div class="dropdown">
-            <button class="dropbtn yellow-bg green-text">Retterwissen</button>
+            <button class="UEBERSCHRIFT">Retterwissen</button>
             <div class="dropdown-content">
-                <a href="https://mwj-mm-05.kurs.jade-hs.de/retterwissen/zukunftsenergien.php" class="green-text">Zukunftsenergie</a>
-                <a href="https://mwj-mm-05.kurs.jade-hs.de/retterwissen/innovationen.php" class="green-text">Innovationen</a>
-                <a href="https://mwj-mm-05.kurs.jade-hs.de/retterwissen/klimaschaeden.php" class="green-text">Klimaschäden</a>
+                <a href="https://mwj-mm-05.kurs.jade-hs.de/retterwissen/zukunftsenergien.php" class="UNTERUEBERSCHRIFT">Zukunftsenergie</a>
+                <a href="https://mwj-mm-05.kurs.jade-hs.de/retterwissen/innovationen.php" class="UNTERUEBERSCHRIFT">Innovationen</a>
+                <a href="https://mwj-mm-05.kurs.jade-hs.de/retterwissen/klimaschaeden.php" class="UNTERUEBERSCHRIFT">Klimaschäden</a>
             </div>
         </div>
-		<a href="https://mwj-mm-05.kurs.jade-hs.de/impressum/impressum.php" class="menu-link yellow-bg green-text">Impressum</a>
+		<a href="https://mwj-mm-05.kurs.jade-hs.de/impressum/impressum.php" class="UEBERSCHRIFT">Impressum</a>
     </div>
 </nav>
 
